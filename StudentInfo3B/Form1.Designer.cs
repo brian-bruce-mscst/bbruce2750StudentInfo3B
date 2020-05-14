7ac7fab156227bcab3390cda3780a99516a4f96f
Brian.Bruce@my.southeastmn.edu <C*lin1109>
2020-05-13 20:47:39 -0500

    completed ex3b

--- StudentInfo3B/Form1.Designer.cs
+++ StudentInfo3B/Form1.Designer.cs
@@ -48,76 +48,90 @@
             // 
             // dateTextBox
             // 
-            this.dateTextBox.Location = new System.Drawing.Point(157, 322);
+            this.dateTextBox.Location = new System.Drawing.Point(209, 396);
+            this.dateTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dateTextBox.Name = "dateTextBox";
-            this.dateTextBox.Size = new System.Drawing.Size(100, 20);
+            this.dateTextBox.Size = new System.Drawing.Size(132, 22);
             this.dateTextBox.TabIndex = 47;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(21, 325);
+            this.label7.Location = new System.Drawing.Point(28, 400);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(67, 13);
+            this.label7.Size = new System.Drawing.Size(85, 17);
             this.label7.TabIndex = 46;
             this.label7.Text = "Points/Date:";
             // 
             // saveGradeButton
             // 
-            this.saveGradeButton.Location = new System.Drawing.Point(263, 322);
+            this.saveGradeButton.Location = new System.Drawing.Point(351, 396);
+            this.saveGradeButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.saveGradeButton.Name = "saveGradeButton";
-            this.saveGradeButton.Size = new System.Drawing.Size(67, 23);
+            this.saveGradeButton.Size = new System.Drawing.Size(89, 28);
             this.saveGradeButton.TabIndex = 45;
             this.saveGradeButton.Text = "Save";
             this.saveGradeButton.UseVisualStyleBackColor = true;
+            this.saveGradeButton.Click += new System.EventHandler(this.saveGradeButton_Click);
             // 
             // pointsTextBox
             // 
-            this.pointsTextBox.Location = new System.Drawing.Point(99, 322);
+            this.pointsTextBox.Location = new System.Drawing.Point(132, 396);
+            this.pointsTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pointsTextBox.Name = "pointsTextBox";
-            this.pointsTextBox.Size = new System.Drawing.Size(52, 20);
+            this.pointsTextBox.Size = new System.Drawing.Size(68, 22);
             this.pointsTextBox.TabIndex = 44;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(21, 234);
+            this.label6.Location = new System.Drawing.Point(28, 288);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(69, 13);
+            this.label6.Size = new System.Drawing.Size(92, 17);
             this.label6.TabIndex = 43;
             this.label6.Text = "Assignments:";
             // 
             // assignmentsListBox
             // 
             this.assignmentsListBox.FormattingEnabled = true;
-            this.assignmentsListBox.Location = new System.Drawing.Point(99, 234);
+            this.assignmentsListBox.ItemHeight = 16;
+            this.assignmentsListBox.Location = new System.Drawing.Point(132, 288);
+            this.assignmentsListBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.assignmentsListBox.Name = "assignmentsListBox";
-            this.assignmentsListBox.Size = new System.Drawing.Size(231, 82);
+            this.assignmentsListBox.Size = new System.Drawing.Size(307, 100);
             this.assignmentsListBox.TabIndex = 42;
+            this.assignmentsListBox.SelectedValueChanged += new System.EventHandler(this.assignmentsListBox_SelectedValueChanged);
             // 
             // enrollmentsListBox
             // 
             this.enrollmentsListBox.FormattingEnabled = true;
-            this.enrollmentsListBox.Location = new System.Drawing.Point(99, 120);
+            this.enrollmentsListBox.ItemHeight = 16;
+            this.enrollmentsListBox.Location = new System.Drawing.Point(132, 148);
+            this.enrollmentsListBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.enrollmentsListBox.Name = "enrollmentsListBox";
-            this.enrollmentsListBox.Size = new System.Drawing.Size(231, 108);
+            this.enrollmentsListBox.Size = new System.Drawing.Size(307, 132);
             this.enrollmentsListBox.TabIndex = 41;
+            this.enrollmentsListBox.SelectedValueChanged += new System.EventHandler(this.enrollmentsListBox_SelectedValueChanged);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(21, 123);
+            this.label5.Location = new System.Drawing.Point(28, 151);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(47, 13);
+            this.label5.Size = new System.Drawing.Size(61, 17);
             this.label5.TabIndex = 40;
             this.label5.Text = "Student:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(21, 96);
+            this.label4.Location = new System.Drawing.Point(28, 118);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.Size = new System.Drawing.Size(59, 17);
             this.label4.TabIndex = 38;
             this.label4.Text = "Section:";
             // 
@@ -125,17 +139,20 @@
             // 
             this.sectionsDropDownList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.sectionsDropDownList.FormattingEnabled = true;
-            this.sectionsDropDownList.Location = new System.Drawing.Point(99, 93);
+            this.sectionsDropDownList.Location = new System.Drawing.Point(132, 114);
+            this.sectionsDropDownList.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.sectionsDropDownList.Name = "sectionsDropDownList";
-            this.sectionsDropDownList.Size = new System.Drawing.Size(170, 21);
+            this.sectionsDropDownList.Size = new System.Drawing.Size(225, 24);
             this.sectionsDropDownList.TabIndex = 39;
+            this.sectionsDropDownList.SelectedValueChanged += new System.EventHandler(this.sectionsDropDownList_SelectedValueChanged);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(21, 69);
+            this.label3.Location = new System.Drawing.Point(28, 85);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.Size = new System.Drawing.Size(45, 17);
             this.label3.TabIndex = 36;
             this.label3.Text = "Term:";
             // 
@@ -143,26 +160,31 @@
             // 
             this.coursesDropDownList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.coursesDropDownList.FormattingEnabled = true;
-            this.coursesDropDownList.Location = new System.Drawing.Point(99, 39);
+            this.coursesDropDownList.Location = new System.Drawing.Point(132, 48);
+            this.coursesDropDownList.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.coursesDropDownList.Name = "coursesDropDownList";
-            this.coursesDropDownList.Size = new System.Drawing.Size(231, 21);
+            this.coursesDropDownList.Size = new System.Drawing.Size(307, 24);
             this.coursesDropDownList.TabIndex = 35;
+            this.coursesDropDownList.SelectedValueChanged += new System.EventHandler(this.coursesDropDownList_SelectedValueChanged);
             // 
             // termsDropDownList
             // 
             this.termsDropDownList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.termsDropDownList.FormattingEnabled = true;
-            this.termsDropDownList.Location = new System.Drawing.Point(99, 66);
+            this.termsDropDownList.Location = new System.Drawing.Point(132, 81);
+            this.termsDropDownList.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.termsDropDownList.Name = "termsDropDownList";
-            this.termsDropDownList.Size = new System.Drawing.Size(170, 21);
+            this.termsDropDownList.Size = new System.Drawing.Size(225, 24);
             this.termsDropDownList.TabIndex = 37;
+            this.termsDropDownList.SelectedValueChanged += new System.EventHandler(this.termsDropDownList_SelectedValueChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(21, 42);
+            this.label2.Location = new System.Drawing.Point(28, 52);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.Size = new System.Drawing.Size(57, 17);
             this.label2.TabIndex = 34;
             this.label2.Text = "Course:";
             // 
@@ -170,25 +192,28 @@
             // 
             this.departmentsDropDownList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.departmentsDropDownList.FormattingEnabled = true;
-            this.departmentsDropDownList.Location = new System.Drawing.Point(99, 12);
+            this.departmentsDropDownList.Location = new System.Drawing.Point(132, 15);
+            this.departmentsDropDownList.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.departmentsDropDownList.Name = "departmentsDropDownList";
-            this.departmentsDropDownList.Size = new System.Drawing.Size(170, 21);
+            this.departmentsDropDownList.Size = new System.Drawing.Size(225, 24);
             this.departmentsDropDownList.TabIndex = 33;
+            this.departmentsDropDownList.SelectedValueChanged += new System.EventHandler(this.departmentsDropDownList_SelectedValueChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(21, 15);
+            this.label1.Location = new System.Drawing.Point(28, 18);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.Size = new System.Drawing.Size(86, 17);
             this.label1.TabIndex = 32;
             this.label1.Text = "Department:";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(355, 360);
+            this.ClientSize = new System.Drawing.Size(473, 443);
             this.Controls.Add(this.dateTextBox);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.saveGradeButton);
@@ -205,6 +230,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.departmentsDropDownList);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Exercise 3B: WinForm & Entity Framework";
             this.Load += new System.EventHandler(this.Form1_Load);
--- StudentInfo3B/StudentInfoModel.Designer.cs
+++ StudentInfo3B/StudentInfoModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'E:\users\sd3699dj\Documents\Visual Studio 2017\Projects\mswanson2750Unit3StudentInfo\StudentInfo3B\StudentInfoModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\e054416\Documents\2750-brian-bruce\bbruce2750StudentInfo3B\StudentInfo3B\StudentInfoModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
